82c82ecff60f08991feb74d3d1c7f34d7e6e2c4a
Ajit Naik <>
2023-10-31 13:56:46 +0100

    Add i18 translation in Amplify Auth

--- src/app/[locale]/shelter/page.tsx
+++ src/app/[locale]/shelter/page.tsx
@@ -1,14 +1,20 @@
 "use client"
-import { Authenticator } from '@aws-amplify/ui-react'
+import { Authenticator, translations } from '@aws-amplify/ui-react'
 import React, {  } from 'react'
-import { Amplify } from 'aws-amplify'
+import { Amplify, I18n } from 'aws-amplify'
 import '@aws-amplify/ui-react/styles.css';
 
 import { awsExports } from '../authentication/Configuration'
 import AuthenticatedShelter from './AuthenticatedShelter'
 import { formFields } from '@/app/types';
 Amplify.configure({ ...awsExports, ssr: true })
-
+I18n.putVocabularies(translations);
+I18n.putVocabularies({
+    de: {
+        'Enter Your Email': 'Geben Sie Ihre E-Mail ein',
+        'Please confirm your Password': 'Bitte bestätigen Sie Ihr Passwort'
+    },
+});
 const Shelter = () => {
   return (
     <div>
