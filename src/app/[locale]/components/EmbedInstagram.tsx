86118eda063a53e21072e65bb8eb1d4bdd11bb9f
Ajit Naik <>
2023-11-05 19:56:07 +0100

    Update Google analytics to use lazyOnload strategy

--- src/app/[locale]/components/EmbedInstagram.tsx
+++ src/app/[locale]/components/EmbedInstagram.tsx
@@ -1,32 +1,33 @@
-import { useEffect, useState } from "react";
 import { InstagramEmbed } from "react-social-media-embed";
 
-const EmbedInstagram = ({url}: {url: string}) => {
+const EmbedInstagram = ({ url }: { url: string }) => {
 
-    const [isMouseOver, setIsMouseOver] = useState(false);
+    // Disable tracking
+    // const [isMouseOver, setIsMouseOver] = useState(false);
 
-    useEffect(() => {
-                // Focus the page
-                window.focus();
-                // Add listener to check when page is not focussed
-                // (i.e. iframe is clicked into)
-                window.addEventListener("blur", onWindowBlur);
+    // useEffect(() => {
+    //             // Focus the page
+    //             window.focus();
+    //             // Add listener to check when page is not focussed
+    //             // (i.e. iframe is clicked into)
+    //             window.addEventListener("blur", onWindowBlur);
 
-                return function cleanup() {
-                    window.removeEventListener("blur", onWindowBlur);
-                }
-      });
+    //             return function cleanup() {
+    //                 window.removeEventListener("blur", onWindowBlur);
+    //             }
+    //   });
 
-      const onWindowBlur = () => {
-        if (isMouseOver) {
-            console.log(url)
-        }
-      };
-    
+    //   const onWindowBlur = () => {
+    //     if (isMouseOver) {
+    //         console.log(url)
+    //     }
+    //   };
+
     return (
-        <div onMouseOver={() => setIsMouseOver(true)} onMouseOut={() => setIsMouseOver(false)}>
-            <InstagramEmbed url={url} width={328}/>
-        </div>
+        <InstagramEmbed url={url} width={328} />
+        // <div onMouseOver={() => setIsMouseOver(true)} onMouseOut={() => setIsMouseOver(false)}>
+        //     <InstagramEmbed url={url} width={328}/>
+        // </div>
     )
 }
 
--- src/app/[locale]/components/GoogleAnalytics.tsx
+++ src/app/[locale]/components/GoogleAnalytics.tsx
@@ -6,9 +6,9 @@ const GoogleAnalytics = () => {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=G-MZCP7Y54KY`}
-        strategy="afterInteractive"
+        strategy="lazyOnload"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics" strategy="lazyOnload">
         {`
         window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
