a8848e853742b51aaa1ddc730a42846ab7b91a81
Ajit Naik <>
2023-11-05 21:57:59 +0100

    Enable localePrefix

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export default createMiddleware({
   // If this locale is matched, pathnames work without a prefix (e.g. `/about`)
   defaultLocale: 'en',
 
-  // localePrefix: 'as-needed'
+  localePrefix: 'as-needed'
 });
  
 export const config = {
